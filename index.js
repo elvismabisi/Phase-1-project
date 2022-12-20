2a2bf4e063def0680f2b04ddb548b48c50100bb1
Gregory <elvismabisi@gmail.com>
2022-12-20 21:12:36 +0300

    complete

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
       const username = loginForm.querySelector('input[name="username"]').value;
       const password = loginForm.querySelector('input[name="password"]').value;
       // Validate username and password
+      if (username === 'admin' && password === 'password') {
+        // Show success message
+        document.querySelector('#login-success-msg').innerText = 'Login successful';
+        document.querySelector('#login-success-msg-holder').style.display = 'block';
+        // Hide error message
+        document.querySelector('#login-error-msg-holder').style.display = 'none';
+      } else {
+        // Show error message
+        document.querySelector('#login-error-msg').innerText = 'Invalid username or password';
+        document.querySelector('#login-error-msg-holder').style.display = 'block';
+        // Hide success message
+        document.querySelector('#login-success-msg-holder').style.display = 'none';
+      }
+    });
+})
